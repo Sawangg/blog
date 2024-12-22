f770e566fc094d2e72c9ab5302c4cb91e2ae19b4
Sawang <leo.mercier1@proton.me>
2024-12-22 22:08:51 +0000

    fix(env): drizzle-kit just *works* & support for staging env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "drizzle-kit";
+import { loadEnv } from "vite";
+
+// NOTE: The modes in this project can be development, staging or production
+const mode = process.env.NODE_ENV;
+if (!mode) throw new Error("Unable to find the current mode");
+const { DB_URL } = loadEnv(mode, process.cwd(), "");
 
 export default {
   schema: "./src/db/schema.ts",
@@ -6,7 +12,7 @@ export default {
   dialect: "postgresql",
   casing: "snake_case",
   dbCredentials: {
-    url: process.env.DB_URL as string,
+    url: DB_URL as string,
   },
   strict: true,
 } satisfies Config;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,4 @@
 import { DB_URL } from "astro:env/server";
 import { drizzle } from "drizzle-orm/neon-http";
 
-// NOTE: We can't use drizzle studio because of astro:env
 export const db = drizzle({ connection: DB_URL, casing: "snake_case" });
