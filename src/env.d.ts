c4d19479ff90f56dff18cfe144505e65ad2f5d08
Sawang <leo.mercier1@proton.me>
2025-01-09 14:00:12 +0100

    fix(env): removed unnecessary astro env.d.ts declarations

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,3 @@
-/// <reference path="../.astro/actions.d.ts" />
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
 declare namespace App {
   interface Locals {
     session: import("@lib/session").Session | null;
